cd33441332b808b2c845d96f561db85c0716304a
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2025-05-08 15:11:25 +0100

    Configure stride experiment

--- experiments/metrics.py
+++ experiments/metrics.py
@@ -4,6 +4,7 @@ import torchvision
 
 from ecopt.meter import CodeCarbonMeter
 from models.nn import NeuralNetworkModel
+from models.cnn import CNNModel
 from ecopt.hyperparameter import Fixed
 
 # load dataset
@@ -19,13 +20,14 @@ eval_dataset = torchvision.datasets.MNIST(
 
 # configure MLflow tracking
 mlflow_tracking_uri = "https://mlflow.emileferreira.com"
+
+# NN
 run_tags = {
     "machine": environ["ECOPT_MACHINE"],
     "rapl": False,
     "model": "nn",
     "dataset": "mnist"
 }
-
 meter = CodeCarbonMeter(
     experiment_name="Metrics",
     mlflow_tracking_uri=mlflow_tracking_uri)
@@ -34,5 +36,17 @@ for depth in range(1, 31):
                                eval_dataset=eval_dataset,
                                depth=Fixed(depth),
                                hidden_size=Fixed(28 * 28 * 10))
+    meter(model, utility_measure="weighted_f1",
+          run_tags=run_tags, skip_train=True)
+
+# CNN
+run_tags["model"] = "cnn"
+meter = CodeCarbonMeter(
+    experiment_name="Metrics",
+    mlflow_tracking_uri=mlflow_tracking_uri)
+for stride in range(2, 11):
+    model = CNNModel(train_dataset=train_dataset,
+                     eval_dataset=eval_dataset,
+                     stride=Fixed(stride))
     meter(model, utility_measure="weighted_f1",
           run_tags=run_tags, skip_train=True)
