14ac7417673d0be985510fda2d28474c2b7e73e5
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2025-05-08 15:10:12 +0100

    Define generic CNN model

--- experiments/models/cnn.py
+++ experiments/models/cnn.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 from sklearn.metrics import f1_score
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+import mlflow
+from thop import profile
 
 from ecopt.model import Model
 from ecopt.hyperparameter import Fixed, Hyperparameter
-
-# TODO: define ResNet model
 
 
 class LeNet5(torch.nn.Module):
@@ -52,8 +52,10 @@ class LeNet5Model(Model):
         self.learning_rate = learning_rate
         self.output_size = output_size
 
-    def train(self):
+    def define(self):
         self.model = LeNet5(num_classes=self.output_size.value).to(self.device)
+
+    def train(self):
         dataloader = DataLoader(dataset=self.train_dataset,
                                 batch_size=self.batch_size.value,
                                 shuffle=True)
@@ -74,6 +76,13 @@ class LeNet5Model(Model):
                     progress.update(1 / len(dataloader))
 
     def evaluate(self) -> (float, int):
+        image = self.eval_dataset[0][0]
+        image = image.view(1, *image.size()).to(self.device)
+        macs, parameters = profile(self.model, inputs=(image,))
+        mlflow.log_metrics({
+            "parameters": parameters,
+            "flops": 2 * macs
+        })
         dataloader = DataLoader(dataset=self.eval_dataset,
                                 batch_size=self.batch_size.value,
                                 shuffle=False)
@@ -88,5 +97,67 @@ class LeNet5Model(Model):
                 y_true.append(labels)
                 y_pred.append(predictions)
         y_true, y_pred = np.concat(y_true), np.concat(y_pred)
-        num_samples = len(self.eval_dataset)
+        num_samples = len(self.eval_dataset) + 1
         return f1_score(y_true, y_pred, average="weighted"), num_samples
+
+
+class CNN(torch.nn.Module):
+
+    def __init__(self, width: int, depth: int, input_width: int,
+                 input_height: int, input_channels: int, kernel_size: int,
+                 stride: int, padding: int, output_size: int):
+        super(CNN, self).__init__()
+        layers = [torch.nn.Conv2d(
+            input_channels, width, kernel_size, stride=stride, padding=padding
+        ), torch.nn.ReLU()]
+        for _ in range(depth - 1):
+            layers += [torch.nn.Conv2d(
+                width, width, kernel_size, stride=stride, padding=padding
+            ), torch.nn.ReLU()]
+        self.feature_extractor = torch.nn.Sequential(*layers)
+        # automatically calculate classifier dimension
+        with torch.no_grad():
+            dummy_input = torch.zeros(
+                2, input_channels, input_height, input_width)
+            dummy_output = self.feature_extractor(dummy_input)
+            _, c, h, w = dummy_output.shape
+            flattened_size = c * h * w
+        self.classifier = torch.nn.Linear(flattened_size, output_size)
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.classifier(x)
+
+
+class CNNModel(LeNet5Model):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def __init__(self, train_dataset: Dataset, eval_dataset: Dataset,
+                 batch_size: Hyperparameter = Fixed(100),
+                 num_epochs: Hyperparameter = Fixed(10),
+                 learning_rate: Hyperparameter = Fixed(0.001),
+                 output_size: Hyperparameter = Fixed(10),
+                 width: Hyperparameter = Fixed(128),
+                 depth: Hyperparameter = Fixed(10),
+                 input_width: Hyperparameter = Fixed(28),
+                 input_height: Hyperparameter = Fixed(28),
+                 input_channels: Hyperparameter = Fixed(1),
+                 kernel_size: Hyperparameter = Fixed(3),
+                 stride: Hyperparameter = Fixed(1),
+                 padding: Hyperparameter = Fixed(1)):
+        super().__init__(train_dataset, eval_dataset, batch_size, num_epochs,
+                         learning_rate, output_size)
+        self.width, self.depth = width, depth
+        self.input_width, self.input_height = input_height, input_width
+        self.input_channels = input_channels
+        self.kernel_size = kernel_size
+        self.stride, self.padding = stride, padding
+
+    def define(self):
+        self.model = CNN(self.width.value, self.depth.value,
+                         self.input_width.value, self.input_height.value,
+                         self.input_channels.value, self.kernel_size.value,
+                         self.stride.value, self.padding.value,
+                         self.output_size.value)
