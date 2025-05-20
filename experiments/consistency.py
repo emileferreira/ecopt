1b08041a129f227f0428f0af07ef233faa1a57da
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2025-05-20 13:39:50 +0100

    Automate RAPL check

--- experiments/consistency.py
+++ experiments/consistency.py
@@ -1,13 +1,15 @@
 from os import environ
 
+from codecarbon.core.cpu import is_rapl_available
 from ecopt.meter import CodeCarbonMeter
 from ecopt.hyperparameter import Fixed
+
 from models.transformer import TextGenerationModel
 
 mlflow_tracking_uri = "https://mlflow.emileferreira.com"
 run_tags = {
     "machine": environ["ECOPT_MACHINE"],
-    "rapl": False,
+    "rapl": is_rapl_available(),
     "model": "textgeneration",
 }
 meter = CodeCarbonMeter(
